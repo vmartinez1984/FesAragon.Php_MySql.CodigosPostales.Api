e90d7dcc68c4cf9253a81475ef9eef802192c53b
Víctor Martinez <ahal_tocob@hotmail.com>
2024-05-15 18:12:23 -0600

    Ajuste para que este en el  mismo nodo de docker

--- v2/repositorios/Conexion.php
+++ v2/repositorios/Conexion.php
@@ -5,9 +5,25 @@ class Conexion{
 	public $mysqli;
 
 	public function __construct(){        
-        $this->mysqli = new mysqli("192.168.1.86:3307", "root", "123456", "codigospostales");
+      try{        
+        $server = "localhost";
+        $server ="192.168.1.86";
+        //$server = "localhost:3306";
+        $usuario = "root";
+        $contrasenia = "123456";
+        $baseDeDatos= "CodigosPostales";
+        // $server = "vmartinez84.xyz:3306";
+        // $usuario = "vmartinez_CodigosPostales";
+        // $contrasenia = "Macross#2012";
+        // $baseDeDatos = "vmartinez_codigos_postales";
+        $puerto = 3306;
+
+        $this->mysqli = new mysqli($server, $usuario, $contrasenia, $baseDeDatos, $puerto);
         if ($this->mysqli->connect_errno) {
             echo "Error en la conexion" . $this->mysqli->connect_error;
         }
+      }catch(Exception $ex){
+        //print_r($ex);
+      }
     }
 }
